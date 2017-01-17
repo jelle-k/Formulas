4201fd41b924a95bf8dad5160c27126318661ef7
Jelle_LC <jelle@lovechock.com>
2017-01-17 16:14:50 +0100

    Tuesday

--- Lovechock/Profitability Sales Analysis.sql
+++ Lovechock/Profitability Sales Analysis.sql
@@ -52,10 +52,32 @@ INV1 T0
 RIGHT OUTER JOIN OINV T1 ON T0."DocEntry" = T1."DocEntry"
 
 WHERE 
-T1."DocDate" BETWEEN [%0] AND [%1] AND 
-T1."CardCode" = 100774
+T1."DocDate" BETWEEN [%0] AND [%1]
+
+GROUP BY T1."CardCode", T1."CardName", T0."ItemCode", T0."Dscription"
+
+UNION
+
+SELECT 
+T1."CardCode",
+T1."CardName",
+T0."ItemCode", 
+T0."Dscription", 
+SUM((T0."PriceAfVAT" * T0."Quantity") * -1) AS "Total Sales Amount", 
+SUM((T0."GrssProfit") * -1) AS "Gross Profit",
+SUM(((T0."PriceAfVAT" * T0."Quantity") - T0."GrssProfit") * -1) AS "Total Base Price for Profit",
+0
+
+FROM 
+RIN1 T0 
+RIGHT OUTER JOIN ORIN T1 ON T0."DocEntry" = T1."DocEntry"
+
+WHERE 
+T1."DocDate" BETWEEN [%0] AND [%1]
 
 GROUP BY T1."CardCode", T1."CardName", T0."ItemCode", T0."Dscription"
 
 ORDER BY
-T1."CardName"
+T1."CardName", T0."ItemCode"
+
+
